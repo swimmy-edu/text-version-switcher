1c5b57349c6c3e179070366ad92f0033808729e4
swimmy001 <swimmy001@sai.co.jp>
2023-09-29 13:38:17 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,129 @@
+const data = {
+  'ch0 はじめに': '新テキストの【CH0】から進めてください。',
+  'ch1 変数の使い方': {
+      'ex1': '新テキストの【CH0】を簡単に確認したあと、【CH1】変数と計算から進めてください。',
+      'ex2': '新テキストの【CH0】を簡単に確認したあと、【CH1】pr01（p.2）から進めてください。',
+      'ex3': '新テキストの【CH0】を簡単に確認したあと、【CH1】(4)データを出力する（p.3）から進めてください。',
+      'ex4': '新テキストの【CH0】を簡単に確認したあと、【CH1】数値の計算（p.4）から進めてください。',
+      'ex5': '新テキストの【CH0】を簡単に確認したあと、【CH1】数値の計算（p.4）から進めてください。',
+      'ex6': '新テキストの【CH0】を簡単に確認したあと、【CH1】数値の計算（p.4）から進めてください。',
+      'pr1': '新テキストの【CH0】を簡単に確認したあと、【CH1】数値の計算（p.4）から進めてください。',
+      'pr2': '新テキストの【CH0】を簡単に確認したあと、【CH1】数値の計算（p.4）から進めてください。',
+      'mc1': '新テキストの【CH0】を簡単に確認したあと、【CH1】Minecraftを制御する（p.7）から進めてください。',
+      'mc2': '新テキストの【CH0】を簡単に確認したあと、【CH1】mc01（p.7）から進めてください。',
+      'mc3': '新テキストの【CH0】を簡単に確認したあと、【CH1】mc02（p.8）から進めてください。',
+      'mc4': '新テキストの【CH0】を簡単に確認したあと、【CH1】mc03（p.9）から進めてください。',
+  },
+  'ch2 変数を使って計算する': {
+      'ex1': '新テキストの【CH1】数値の計算（p.4）からpr03（p.7）まで取り組みましょう。\n その後は【CH1】mc04（p.9）から進めてください。',
+      'ex2': '新テキストの【CH1】pr02（p.6）からpr03（p.7）まで取り組みましょう。\n その後は【CH1】mc04（p.9）から進めてください。',
+      'ex3': '新テキストの【CH1】eg05（p.6）からpr03（p.7）まで取り組みましょう。\n その後は【CH1】mc04（p.9）から進めてください。',
+      'ex4': '新テキストの【CH1】pr03（p.7）に取り組みましょう。\n その後は【CH1】mc04（p.9）から進めてください。',
+      'ex5': '新テキストの【CH1】mc04（p.9）から進めてください。',
+      'ex6': '新テキストの【CH1】mc04（p.9）から進めてください。',
+      'ex7': '新テキストの【CH1】mc04（p.9）から進めてください。',
+      'pr1': '新テキストの【CH1】mc04（p.9）から進めてください。\n\n 【CH1】ex03（p.14）はスキップしてもOKです。',
+      'pr2': '新テキストの【CH1】mc04（p.9）から進めてください。\n\n 【CH1】ex03（p.14）はスキップしてもOKです。',
+      'mc1': '新テキストの【CH1】mc04（p.9）から進めてください。\n\n 【CH1】ex03（p.14）はスキップしてもOKです。',
+      'mc2': '新テキストの【CH1】mc05（p.10）から進めてください。\n\n 【CH1】ex03（p.14）はスキップしてもOKです。',
+      'mc3': '新テキストの【CH1】ex01（p.13）から進めてください。\n\n 【CH1】ex03（p.14）はスキップしてもOKです。',
+      'mc4': '新テキストの【CH1】ex01（p.13）から進めてください。\n\n 【CH1】ex03（p.14）はスキップしてもOKです。',
+  },
+  'ch3 変数や文字列を結合する': '新テキストの【CH2】から進めてください。',
+  'ch4 ifを使って条件分岐': {
+    'ex1': '新テキストの【CH2】から進めてください。',
+    'ex2': '新テキストの【CH2】から進めてください。\n\n【CH4】eg02（p.5）は学習済です。すでに理解できているところは適宜スキップしてもOKです。',
+    'ex3': '新テキストの【CH2】から進めてください。\n\n【CH4】eg02（p.5）, pr01（p.7）は学習済です。すでに理解できているところは適宜スキップしてもOKです。',
+    'ex4': '新テキストの【CH2】から進めてください。\n\n【CH4】eg02（p.5）〜eg03（p.7）は学習済です。すでに理解できているところは適宜スキップしてもOKです。',
+    'pr1': '新テキストの【CH2】から進めてください。\n\n【CH4】条件分岐（pp.3〜8）はスキップしてもOKです。',
+    'pr2': '新テキストの【CH2】から進めてください。\n\n【CH4】条件分岐（pp.3〜8）はスキップしてもOKです。',
+    'pr3': '新テキストの【CH2】から進めてください。\n\n【CH4】条件分岐（pp.3〜8）とex03（p.23）はスキップしてもOKです。',
+    'mc1': '新テキストの【CH2】から進めてください。\n\n【CH4】条件分岐（pp.3〜8）とex01（p.22）, ex03（p.23）はスキップしてもOKです。',
+    'mc2': '新テキストの【CH2】から進めてください。\n\n【CH4】条件分岐（pp.3〜8）とmc01（p.17）, ex01（p.22）, ex03（p.23）はスキップしてもOKです。',
+  },
+  'ch5 繰り返し文（while）': {
+    'ex1': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。',
+    'ex2': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。',
+    'ex3': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。',
+    'ex4': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらsleep関数（p.11）から進めてください。',
+    'ex5': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらsleep関数（p.11）から進めてください。',
+    'ex6': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらsleep関数（p.11）から進めてください。',
+    'pr1': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらrandint関数（p.14）から進めてください。',
+    'pr2': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらrandint関数（p.14）から進めてください。',
+    'pr3': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらrandint関数（p.14）から進めてください。',
+    'pr4': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらrandint関数（p.14）から進めてください。',
+    'mc1': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらrandint関数（p.14）から進めてください。',
+    'mc2': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらrandint関数（p.14）から進めてください。\n\n【CH5】mc01（p.16）はスキップしてもOKです。',
+    'mc3': '新テキストの【CH2】から進めてください。\n\n【CH4】では、オブジェクトの比較（p.1）・論理演算（p.9）・match文（p.16）について学習し、【CH5】に進みましょう。\n\n【CH5】に入ったらrandint関数（p.14）から進めてください。\n\n【CH5】mc01（p.16）, mc02（p.17）はスキップしてもOKです。',
+  },
+  'ch6 繰り返し文（for）': '未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n\n上記の学習が終わったら、【CH6】から進めてください。',
+  'ch7 ユーザ定義関数': {
+    'ex1〜4': '未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n・【CH6】リスト\n\n上記の学習が終わったら、【CH7】から進めてください。',
+    'pr1〜mc1': '未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数の復習\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n・【CH6】リスト\n\n上記の学習が終わったら、【CH7】から進めてください。',
+    'mc2': '未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数の復習\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n・【CH6】リスト\n\n上記の学習が終わったら、【CH7】から進めてください。\n【CH7】mc03（p.17）は学習済です。',
+    'mc3': '未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数の復習\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n・【CH6】リスト\n\n上記の学習が終わったら、【CH7】から進めてください。\n【CH7】mc03〜mc04（p.17）は学習済です。',
+    'mc4': '未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数の復習\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n・【CH6】リスト\n\n上記の学習が終わったら、【CH7】から進めてください。\n【CH7】mc03〜mc04（p.17）, ex04（p.25）は学習済です。'
+  },
+  'ch8〜9': '未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数の復習\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n・【CH6】リスト\n\n上記の学習が終わったら、【CH7】から進めてください。\n【CH7】mc03〜mc04（p.17）, ex04（p.25）, 【CH8】ex14（p.8）は学習済です。\nその他やったことがある問題はスキップしてOKです。',
+  'ものこと編': '現在進めている章が終わったら、未学習の範囲を新テキストで学習していきましょう。\n\n・【CH2】型と組み込み関数\n・【CH3】ユーザー定義関数の復習\n・【CH4】比較演算子・論理演算子・match文\n・【CH5】randint関数\n・【CH6】リスト\n・【CH7】タプルと辞書\n\n上記の学習が終わったらものこと編の続きから進めてください。\n【CH7】mc03〜mc04（p.17）, ex04（p.25）, 【CH8】ex14（p.8）は学習済です。'
+};
+
+const chapterSelect = document.getElementById('chapterSelect');
+const exerciseSelect = document.getElementById('exerciseSelect');
+const guide = document.getElementById('guide');
+
+// チャプターの選択肢を初期化
+for (const chapter in data) {
+  const option = document.createElement('option');
+  option.value = chapter;
+  option.textContent = chapter;
+  chapterSelect.appendChild(option);
+}
+
+chapterSelect.addEventListener('change', (e) => {
+  // 問題番号の選択肢と案内文をリセット
+  exerciseSelect.innerHTML = '<option value="">次回学習予定の問題番号を選択してください</option>';
+  guide.textContent = '';  // 案内文をリセット
+
+  const selectedChapter = e.target.value;
+
+  // ch3が選択された場合、直接案内文を表示
+  if (selectedChapter === 'ch8〜9') {
+      guide.innerHTML = data['ch8〜9'].replace(/\n/g, '<br>');
+      exerciseSelect.innerHTML = '';  // 問題番号の選択肢をクリア
+      return;
+  }else if (selectedChapter === 'ch0 はじめに') {
+    guide.innerHTML = data['ch0 はじめに'].replace(/\n/g, '<br>');
+    exerciseSelect.innerHTML = '';
+    return;
+  }else if (selectedChapter === 'ch3 変数や文字列を結合する') {
+    guide.innerHTML = data['ch3 変数や文字列を結合する'].replace(/\n/g, '<br>');
+    exerciseSelect.innerHTML = '';
+    return;
+  }else if (selectedChapter === 'ch6 繰り返し文（for）') {
+    guide.innerHTML = data['ch6 繰り返し文（for）'].replace(/\n/g, '<br>'); 
+    exerciseSelect.innerHTML = '';
+    return;
+  }else if (selectedChapter === 'ものこと編') {
+    guide.innerHTML = data['ものこと編'].replace(/\n/g, '<br>'); 
+    exerciseSelect.innerHTML = '';
+    return;
+  }
+
+  for (const exercise in data[selectedChapter]) {
+      const option = document.createElement('option');
+      option.value = exercise;
+      option.textContent = exercise;
+      exerciseSelect.appendChild(option);
+  }
+});
+
+exerciseSelect.addEventListener('change', (e) => {
+  const selectedChapter = chapterSelect.value;
+  const selectedExercise = e.target.value;
+  guide.innerHTML = data[selectedChapter][selectedExercise].replace(/\n/g, '<br>');
+});
+
+function handlePrint() {
+  window.print();
+}
